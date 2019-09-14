2cecdc0de243ca6c596cc0e87d011dacad26dce9
inql <dbinkus96@gmail.com>
2019-09-14 22:53:54 +0200

    Add function to gather current temperature

--- fan_control.py
+++ fan_control.py
@@ -1,2 +1,19 @@
 #!/usr/bin/env python3
-print("Hello, world!")
+from gpiozero import OutputDevice
+import subprocess
+import time
+def get_temperature():
+    """
+    Get the temperature of raspberry pi
+    :return:
+    """
+    gather_output = subprocess.run(['vcgencmd', 'measure_temp'], capture_output=True)
+    current_temperature = gather_output.stdout.decode()
+    try:
+        return float(current_temperature.split('=')[1].split('\'')[0])
+    except (IndexError, ValueError):
+        raise RuntimeError('Could not parse temperature output.')
+
+if __name__ == '__main__':
+    temp = get_temperature()
+    print(temp)
